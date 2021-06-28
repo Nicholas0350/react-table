b619edade3d83fe02100b19aa2e59d258cdce5b2
n <nicholas@zainawe.com>
2021-06-28 12:34:54 +1000

    table rendering

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,38 +1,54 @@
-
-
+import { useMemo } from 'react';
+import { useTable } from 'react-table';
 import MOCK_DATA from './MOCK_DATA.json'
 import { COLUMNS } from "./columns";
-import { useTable } from 'react-table';
-import { useMemo } from 'react';
-
-
-
+import './table.css'
 
 export const BasicTable = () => {
 
-  const columns = useMemo(() => COLUMN, [])
+  const columns = useMemo(() => COLUMNS, [])
   const data = useMemo(() => MOCK_DATA, [])
 
-  const tableInstance = useTable({
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
     columns,
     data
   })
+
   return (
     <div>
-      <table>
+      <table {...getTableProps()}>
         <thead>
-          <tr>
-            <th></th>
-          </tr>
+          {
+            headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {
+                  headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps}>{column.render('Header')}</th>
+                  ))}
+              </tr>
+            ))}
         </thead>
-        <tbody>
-          <tr>
-            <td></td>
-          </tr>
+        <tbody {...getTableBodyProps()}>
+          {
+            rows.map(row => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {
+                    row.cells.map((cell) => {
+                      return <td {...cell.getCellProps()}>{cell.render('Cell')} </td>
+                    })}
+                </tr>
+              )
+            })}
         </tbody>
-
       </table>
-
     </div>
   )
 }
